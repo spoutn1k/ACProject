7221932affdcf9d90ad4f0c75e78f59def58151f
ClemGavoille <gavoilleclem@gmail.com>
2019-10-15 14:52:46 +0200

    Added some warning

--- src/warn.h
+++ src/warn.h
@@ -3,3 +3,11 @@
 void warn(basic_block bb, const char* error) {
 	printf("[WARNING] %s in basic block %d\n", error, bb->index);
 }
+
+void warning(const char* name,const char* warning) {
+	printf("[WARNING] in function %s : %s\n",name,warning);
+}
+
+void warningline(const char* name,const char* warning,int line) {
+	printf("[WARNING] in function %s at line l.%i: %s\n",name,line,warning);
+}
